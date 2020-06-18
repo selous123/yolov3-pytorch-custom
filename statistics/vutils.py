385bd087c9a1db48c6efd6245a13186f6da2cc64
selous <lrhselous@163.com>
2020-06-18 10:42:22 +0800

    增加数据分析的代码

--- statistics/vutils.py
+++ statistics/vutils.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw
-from my_utils.utils import *
+import torch
+import numpy as np
 def tensor2PIL(t, m = 255):
     if m == 1:
         t = t * 255
@@ -7,11 +8,27 @@ def tensor2PIL(t, m = 255):
     img = Image.fromarray(t.astype(np.uint8))
     return img
 
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+def load_classes(path = "/store/dataset/rubbish_yolo/classes.names"):
+    """
+    Loads class labels at 'path'
+    """
+    fp = open(path, "r")
+    names = fp.read().strip().split("\n")
+    return names
 
 """
 Param:
     image: PIL.Image
-    box: array
+    box: array, xyxy
 """
 def show_box_on_image(image, boxes):
     draw = ImageDraw.Draw(image)
